87a930bef101b9702b0d5f3e9f9448d5214cb9b5
Marcus Georgievski <marcusgeorgievski@outlook.com>
2024-08-14 17:58:59 -0400

    edit

--- src/components/messages/messages.tsx
+++ src/components/messages/messages.tsx
@@ -1,7 +1,9 @@
 import { getMessages } from "@/actions/message";
 import MessageList from "@/components/messages/message-list";
 
-export default async function Messages() {
+export default async function Messages(p: any) {
+  console.log("HERE\n", p);
+
   const messages = await getMessages();
 
   return <MessageList messages={messages.reverse()} />;
